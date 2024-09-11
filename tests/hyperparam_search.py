490775aa84685f3ea3d432de635579bb63e21e98
pkicki <piotr.m.kicki@gmail.com>
2024-09-11 20:47:47 +0200

    Launch experiments on cluster

--- tests/hyperparam_search.py
+++ tests/hyperparam_search.py
@@ -1,32 +1,51 @@
 import optuna
-from experiment import experiment
-#import sqlite3
+from experiment import experiment as mppi_experiment
+from experiment_launcher import single_experiment, run_experiment
 
-env_name = "half_cheetah"
-horizon = 30
-n_samples = 100
 
-def objective(trial):
-    #x = trial.suggest_float('x', -10, 10)
-    noise_sigma = trial.suggest_float('noise_sigma', 0.1, 10.)
-    noise_cutoff_freq = trial.suggest_float('noise_cutoff_freq', 0.1, 10.)
-    mean_reward = experiment(env=env_name,
-                             n_episodes=100,
-                             horizon=horizon,
-                             n_samples=n_samples,
-                             noise_sigma=noise_sigma,
-                             noise_beta=None,
-                             noise_cutoff_freq=noise_cutoff_freq,
-                             render=False,
-                             seed=444,
-                             results_dir="./results")
+@single_experiment
+def experiment(
+    env_name: str = "half_cheetah",
+    n_episodes: int = 100,
+    horizon: int = 30,
+    n_samples: int = 100,
+    alg: str = "fcem",
+    results_dir: str = "./results",
+    seed: int = 444,
+    ):
 
-    return -mean_reward
+    def objective(trial):
+        #x = trial.suggest_float('x', -10, 10)
+        noise_sigma = trial.suggest_float('noise_sigma', 0.1, 10.)
+        if alg == "icem":
+            noise_beta = trial.suggest_float('noise_beta', 0.1, 10.)
+            noise_cutoff_freq = None
+        elif alg == "fcem":
+            noise_beta = None
+            noise_cutoff_freq = trial.suggest_float('noise_cutoff_freq', 0.1, 10.)
+        else:
+            raise ValueError("Unknown algorithm")
+        mean_reward = mppi_experiment(env=env_name,
+                                      n_episodes=n_episodes,
+                                      horizon=horizon,
+                                      n_samples=n_samples,
+                                      noise_sigma=noise_sigma,
+                                      noise_beta=noise_beta,
+                                      noise_cutoff_freq=noise_cutoff_freq,
+                                      render=False,
+                                      seed=seed,
+                                      results_dir="./results")
 
-db_name = "hyperparameter_search.db"
-#con = sqlite3.connect(db_name)
-storage_name = f"sqlite:///{db_name}"
-study = optuna.create_study(study_name=f"{env_name}_h{horizon}_ns{n_samples}_fcem", storage=storage_name, load_if_exists=True)
-#study = optuna.load_study(study_name="distributed-example", storage=storage_name)
-study.optimize(objective, n_trials=10)
-print(study.best_params)  # E.g. {'x': 2.002108042}
+        return mean_reward
+
+    db_name = "hyperparameter_search.db"
+    #db_name = "test.db"
+    storage_name = f"sqlite:///{db_name}"
+    study = optuna.create_study(study_name=f"{env_name}_h{horizon}_ns{n_samples}_{alg}",
+                                storage=storage_name, load_if_exists=True,
+                                direction="maximize")
+    study.optimize(objective, n_trials=10)
+    print(study.best_params)
+
+if __name__ == "__main__":
+    run_experiment(run)
---
+++ tests/launch_hyperparam_search.py
@@ -0,0 +1,58 @@
+from itertools import product
+
+from experiment_launcher import Launcher, is_local
+import os
+
+os.environ["WANDB_API_KEY"] = "a9819ac569197dbd24b580d854c3041ad75efafd"
+
+LOCAL = is_local()
+TEST = False
+USE_CUDA = False
+
+N_SEEDS = 10
+if LOCAL:
+    N_EXPS_IN_PARALLEL = 4
+else:
+    N_EXPS_IN_PARALLEL = 80
+
+N_CORES = 1
+MEMORY_SINGLE_JOB = 2000
+MEMORY_PER_CORE = N_EXPS_IN_PARALLEL * MEMORY_SINGLE_JOB // N_CORES
+PARTITION = 'standard'  # 'amd', 'rtx'
+GRES = 'gpu:1' if USE_CUDA else None  # gpu:rtx2080:1, gpu:rtx3080:1
+CONDA_ENV = "mpc"
+
+experiment_name = 'hyperparam_search'
+
+launcher = Launcher(
+    exp_name=experiment_name,
+    exp_file='hyperparam_search',
+    n_seeds=N_SEEDS,
+    n_exps_in_parallel=N_EXPS_IN_PARALLEL,
+    n_cores=N_CORES,
+    memory_per_core=MEMORY_PER_CORE,
+    days=6,
+    hours=23,
+    minutes=59,
+    seconds=0,
+    partition=PARTITION,
+    gres=GRES,
+    conda_env=CONDA_ENV,
+    use_timestamp=True,
+    compact_dirs=False
+)
+
+algs = ["fcem", "icem"]
+n_sampless = [100]
+horizons = [30]
+
+#for mu_lr, value_lr, constraint_lr in product(mu_lrs, value_lrs, constraint_lrs):
+for alg, n_samples, horizon in product(algs, n_sampless, horizons):
+    launcher.add_experiment(
+        alg__=alg,
+        n_samples__=n_samples,
+        horizon__=horizon,
+        n_episodes=1,
+        env_name="half_cheetah",
+    )
+launcher.run(LOCAL, TEST)
