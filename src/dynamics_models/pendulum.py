8afe2324385ebe5ff49496f611565a4c17e47bdf
pkicki <piotr.m.kicki@gmail.com>
2024-12-10 14:32:58 +0100

    Fixed pendulum model

--- src/dynamics_models/pendulum.py
+++ src/dynamics_models/pendulum.py
@@ -8,8 +8,8 @@ def angle_normalize(x):
 
 
 class Pendulum:
-    action_low = -2.0
-    action_high = 2.0
+    action_low = np.array([-2.0])
+    action_high = np.array([2.0])
     dt = 0.05
 
     def dynamics(self, state, perturbed_action):
@@ -18,6 +18,9 @@ class Pendulum:
         sin_th = state[:, 1].view(-1, 1)
         thdot = state[:, 2].view(-1, 1)
         th = np.arctan2(sin_th, cos_th)
+
+        cost = angle_normalize(th) ** 2 + 0.1 * thdot ** 2 + 0.001 * (perturbed_action**2)
+        cost = cost.squeeze(1)
 
         g = 10
         m = 1
@@ -30,19 +33,10 @@ class Pendulum:
         newthdot = np.clip(newthdot, -8, 8)
         newth = th + newthdot * self.dt
 
+
         #state = torch.cat((newth, newthdot), dim=1)
         state = torch.cat((np.cos(newth), np.sin(newth), newthdot), dim=1)
-        return state, None
-
-    def running_cost(self, state, action):
-        cos_th = state[:, 0]
-        sin_th = state[:, 1]
-        theta = np.arctan2(sin_th, cos_th)
-        theta_dt = state[:, 2]
-        action = action[:, 0]
-        #cost = angle_normalize(theta) ** 2 + 0.1 * theta_dt ** 2
-        cost = angle_normalize(theta) ** 2 + 0.1 * theta_dt ** 2 + 0.001 * (action**2)
-        return cost
+        return state, cost
 
     def train(self, new_data):
         pass
