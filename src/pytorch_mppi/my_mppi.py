dd15fd36cc197db0b780b877720f76363c821461
pkicki <piotr.m.kicki@gmail.com>
2024-12-20 08:20:59 +0100

    Dummy for to make multiple iterations available

--- src/pytorch_mppi/my_mppi.py
+++ src/pytorch_mppi/my_mppi.py
@@ -74,50 +74,77 @@ class MPPI:
         """
         Compute the optimal control sequence using MPPI.
         """
-        # Sample perturbations
-        perturbations = self.noise_dist.sample((self.num_samples, self.action_space_horizon))
+        for i in range(1): # TODO make this a parameter
+            # Sample perturbations
+            perturbations = self.noise_dist.sample((self.num_samples, self.action_space_horizon))
 
-        # Generate sampled control sequences
-        control_samples = self.U.unsqueeze(0) + perturbations  # Shape: (num_samples, horizon, control_dim)
+            # Generate sampled control sequences
+            control_samples = self.U.unsqueeze(0) + perturbations  # Shape: (num_samples, horizon, control_dim)
 
-        # clamp the control samples to the action space
-        clamped_control_samples = torch.clamp(control_samples, torch.tensor(self.env.action_low[None, None]),
-                                      torch.tensor(self.env.action_high[None, None]))
+            # clamp the control samples to the action space
+            clamped_control_samples = torch.clamp(control_samples, torch.tensor(self.env.action_low[None, None]),
+                                        torch.tensor(self.env.action_high[None, None]))
 
-        # interpolate if needed
-        if self.interpolate_nodes:
-            clamped_perturbations = clamped_control_samples - self.U.unsqueeze(0)
-            clamped_control_samples = self.spline_N @ clamped_control_samples
-            #import matplotlib.pyplot as plt
-            #t = torch.linspace(0, 1, self.horizon)
-            #tcps = torch.linspace(0, 1, self.action_space_horizon)
-            #clamped_control_samples_ = self.spline_N @ clamped_control_points
-            #plt.subplot(121)
-            #plt.plot(tcps, self.U[:, 0].cpu().numpy())
-            #plt.plot(tcps, clamped_control_points[0, :, 0].cpu().numpy())
-            #plt.plot(t, clamped_control_samples[0, :, 0].cpu().numpy())
-            #plt.plot(t, clamped_control_samples_[0, :, 0].cpu().numpy())
-            #plt.subplot(122)
-            #plt.plot(tcps, self.U[:, 1].cpu().numpy())
-            #plt.plot(tcps, clamped_control_points[0, :, 1].cpu().numpy())
-            #plt.plot(t, clamped_control_samples[0, :, 1].cpu().numpy())
-            #plt.plot(t, clamped_control_samples_[0, :, 1].cpu().numpy())
-            #plt.show()
-        else:
-            clamped_perturbations = clamped_control_samples - self.U.unsqueeze(0)
+            # interpolate if needed
+            if self.interpolate_nodes:
+                clamped_perturbations = clamped_control_samples - self.U.unsqueeze(0)
+                clamped_control_samples = self.spline_N @ clamped_control_samples
+                #import matplotlib.pyplot as plt
+                #t = torch.linspace(0, 1, self.horizon)
+                #tcps = torch.linspace(0, 1, self.action_space_horizon)
+                #clamped_control_samples_ = self.spline_N @ clamped_control_points
+                #plt.subplot(121)
+                #plt.plot(tcps, self.U[:, 0].cpu().numpy())
+                #plt.plot(tcps, clamped_control_points[0, :, 0].cpu().numpy())
+                #plt.plot(t, clamped_control_samples[0, :, 0].cpu().numpy())
+                #plt.plot(t, clamped_control_samples_[0, :, 0].cpu().numpy())
+                #plt.subplot(122)
+                #plt.plot(tcps, self.U[:, 1].cpu().numpy())
+                #plt.plot(tcps, clamped_control_points[0, :, 1].cpu().numpy())
+                #plt.plot(t, clamped_control_samples[0, :, 1].cpu().numpy())
+                #plt.plot(t, clamped_control_samples_[0, :, 1].cpu().numpy())
+                #plt.show()
+            else:
+                clamped_perturbations = clamped_control_samples - self.U.unsqueeze(0)
 
+            # Simulate trajectories and compute costs
+            self.costs = torch.zeros(self.num_samples, device=self.device)
+            current_states = torch.tile(state[None], (self.num_samples, 1))
+            self.states = torch.zeros((self.num_samples, self.horizon + 1, self.state_dim), device=self.device)
+            self.states[:, 0] = current_states
+            # hotfix for setting s to good value
+            if s is not None:
+                self.env.env.unwrapped.simulator.set_s(s)
+            x = []
+            #gamma = 0.98
+            gamma = 1.0
+            for t in range(self.horizon):
+                #current_control = control_samples[:, t]
+                current_control = clamped_control_samples[:, t]
+                next_states, cost = self.dynamics(current_states, current_control)
+                self.costs += cost * gamma ** t
+                current_states = next_states
+                self.states[:, t + 1] = current_states
+                x.append(cost)
             self.costs += self.terminal_cost(current_states)
+                        
+            
+            #import matplotlib.pyplot as plt
+            #x = torch.stack(x)
+            #for i in range(x.shape[1]):
+            #    plt.plot(x[:, i])
+            #plt.show()
 
-        
-        # Compute weights
-        min_cost = self.costs.min()
-        weights = torch.exp(-1 / self.lambda_ * (self.costs - min_cost))
-        weights /= weights.sum()  # Normalize weights
-        
-        # Update control sequence
-        #weighted_control_update = (weights.unsqueeze(1).unsqueeze(2) * perturbations).sum(dim=0)
-        weighted_control_update = (weights.unsqueeze(1).unsqueeze(2) * clamped_perturbations).sum(dim=0)
-        self.U = self.U + weighted_control_update
+            
+            # Compute weights
+            min_cost = self.costs.min()
+            weights = torch.exp(-1 / self.lambda_ * (self.costs - min_cost))
+            weights /= weights.sum()  # Normalize weights
+            
+            # Update control sequence
+            #weighted_control_update = (weights.unsqueeze(1).unsqueeze(2) * perturbations).sum(dim=0)
+            weighted_control_update = (weights.unsqueeze(1).unsqueeze(2) * clamped_perturbations).sum(dim=0)
+            self.U = self.U + weighted_control_update
 
         current_control = self.U[0]
         if self.shift_nominal_trajectory:
@@ -128,6 +155,7 @@ class MPPI:
 def run_mppi(mppi, env, iter=1000, render=True):
     dataset = torch.zeros((iter, mppi.state_dim + mppi.control_dim), device=mppi.device)
     total_reward = 0
+    info = {"progress": 0.}
     if hasattr(env, "default_controls"):
         mppi.reset(env.default_controls(mppi.horizon)[0])
     else:
@@ -135,11 +163,17 @@ def run_mppi(mppi, env, iter=1000, render=True):
     for i in range(iter):
         #print("Time step", i)
         state = env.get_state()
+        t0 = perf_counter()
         if hasattr(env.unwrapped, "simulator") and hasattr(env.unwrapped.simulator, "last_s"):
             action = mppi.command(state, s=env.unwrapped.simulator.last_s)
         else:
             action = mppi.command(state)
+        t1 = perf_counter()
+        #print("Controller time:", t1 - t0)
+        #action = action * 0.
         res = env.step(action.cpu().numpy())
+        if "progress" in res[3].keys():
+            info["progress"] += res[3]["progress"]
 
         s, r = res[0], res[1]
         total_reward += r
@@ -149,4 +183,4 @@ def run_mppi(mppi, env, iter=1000, render=True):
         dataset[i, :mppi.state_dim] = torch.tensor(state, dtype=mppi.U.dtype)
         dataset[i, mppi.state_dim:] = action
 
-    return total_reward, dataset
+    return total_reward, dataset, info
